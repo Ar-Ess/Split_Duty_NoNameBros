5a0de41d68382d492d56742bbdee7bc0c3d39fb5
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2021-05-02 17:53:31 +0200

    level up scene files created

---
+++ Game/Source/LevelUp.cpp
@@ -0,0 +1,38 @@
+#include "App.h"
+
+#include "Scene.h"
+#include "Textures.h"
+#include "GuiManager.h"
+#include "GuiButton.h"
+#include "GuiString.h"
+#include "World.h"
+#include "EntityManager.h"
+
+#include "LevelUp.h"
+#include "Enemy.h"
+#include "Player.h"
+#include "Collider.h"
+#include "Player.h"
+
+#include "Log.h"
+
+LevelUp::LevelUp()
+{
+
+}
+
+void LevelUp::Start()
+{
+}
+
+void LevelUp::Restart()
+{
+}
+
+void LevelUp::Update()
+{
+}
+
+void LevelUp::Draw()
+{
+}
---
+++ Game/Source/LevelUp.h
@@ -0,0 +1,52 @@
+#ifndef __LEVELUP_H__
+#define __LEVELUP_H__
+
+#include "Point.h"
+#include "SString.h"
+#include "Animation.h"
+#include "Scene.h"
+#include "GuiManager.h"
+#include "GuiButton.h"
+
+#include "Collider.h"
+
+#define MAGENTA {190,0,0,150}
+#define RED {255,0,0,255}
+#define SOFT_RED {240,80,0,255}
+#define BLUE {0,143,255,255}
+#define CYAN {0,255,247,255}	
+
+class Player;
+class Scene;
+class Collision;
+class Animation;
+struct SDL_Texture;
+struct SDL_Rect;
+class GuiString;
+
+
+
+class LevelUp
+{
+public:
+
+    LevelUp();
+
+    void Start();
+
+    void Restart();
+
+    void Update();
+
+    void Draw();
+
+  
+
+
+
+
+};
+
+#endif // __INVENTORY_H__
+
+
