220562c0a4cbaf664bea8b494b4c0fa1353d0139
BooStarGamer <marti.buxeda@gmail.com>
2021-05-13 17:17:27 +0200

    Revert "items logic fixed"
    This reverts commit 04a6ed98b687c003b16c5d20e7944b3c61644405.

--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -18,9 +18,13 @@ Player::Player(SDL_Rect collCombat, EntityType enType) : Entity(enType)
 
     colliderCombat = collCombat;
 
-    playerInventory = new PlayerInventory();
-    playerInventory->Reset();
-    playerInventory->AddItem(ItemType::COINS_I, 3);
+    smallMeatCount = 0;
+    largeMeatCount = 0;
+    featherCount = 0;
+    mantisRodCount = 0;
+    splitedEnemyCount = 0;
+    moneyCount = 0;
+
     //stats
     health=
 
@@ -37,8 +41,9 @@ Player::Player(SDL_Rect collCombat, EntityType enType) : Entity(enType)
         colliderCombat.y = INIT2_COMBAT_POSY;
     }
 
-    
-    
+    playerInventory = new PlayerInventory();
+    playerInventory->AddItem(ItemType::FEATHER_I, 2);
+    playerInventory->AddItem(ItemType::COINS_I, 2);
 
     if (enType == EntityType::PLAYER1)
     {
@@ -155,7 +160,7 @@ void Player::SetUp(short int healthx, short int maxHealthx,short int strengthx, 
     stabStat = stabx;
     lvl = lvlx;
     exp = expx;
-    playerInventory->moneyCount = moneyx;
+    moneyCount = moneyx;
 }
 
 void Player::SetTexture(SDL_Texture *tex)
@@ -165,12 +170,12 @@ void Player::SetTexture(SDL_Texture *tex)
 
 void Player::ItemSetup(int xsmallMeat, int xlargeMeat, int xfeather, int xmantisLeg, int xsplitedEnemy, int xmoney)
 {
-    playerInventory->smallMeatCount = xsmallMeat;
-    playerInventory->largeMeatCount = xlargeMeat;
-    playerInventory->featherCount = xfeather;
-    playerInventory->mantisRodCount = xmantisLeg;
-    playerInventory->splitedEnemyCount = xsplitedEnemy;
-    playerInventory->moneyCount = xmoney;
+    smallMeatCount = xsmallMeat;
+    largeMeatCount = xlargeMeat;
+    featherCount = xfeather;
+    mantisRodCount = xmantisLeg;
+    splitedEnemyCount = xsplitedEnemy;
+    moneyCount = xmoney;
 }
 
 void Player::Jump()
@@ -246,12 +251,12 @@ void Player::RestartPlayer()
     app->scene->player1->colliderWorld = { INIT_PLAYER_WORLD_X, INIT_PLAYER_WORLD_Y, INIT_PLAYER_WORLD_W, INIT_PLAYER_WORLD_H };
     app->scene->player1->collisionRect = { INIT_PLAYER_WORLD_X, INIT_PLAYER_WORLD_Y + 56, INIT_PLAYER_WORLD_W, INIT_PLAYER_WORLD_H - 56 };
 
-    playerInventory->smallMeatCount = 0;
-    playerInventory->largeMeatCount = 0;
-    playerInventory->featherCount = 0;
-    playerInventory->mantisRodCount = 0;
-    playerInventory->splitedEnemyCount = 0;
-    playerInventory->moneyCount = 0;
+    smallMeatCount = 0;
+    largeMeatCount = 0;
+    featherCount = 0;
+    mantisRodCount = 0;
+    splitedEnemyCount = 0;
+    moneyCount = 0;
 
     if (type == EntityType::PLAYER1)
     {
