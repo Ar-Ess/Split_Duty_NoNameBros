da2ef8b206875c12d7b278efffdb6ebaa7cddd68
BooStarGamer <marti.buxeda@gmail.com>
2021-05-15 19:08:39 +0200

    Restored LevelUp file

--- Game/Source/LevelUp.cpp
+++ Game/Source/LevelUp.cpp
@@ -20,23 +20,64 @@
 
 LevelUp::LevelUp()
 {	
-	interfaceTexture = app->tex->Load("Assets/Textures/UI/level-up.png");
 }
 
-void LevelUp::Start()
+void LevelUp::Start(short int exp)
 {
 	SetText();
+
+	interfaceTexture = app->tex->Load("Assets/Textures/UI/level-up.png");
+
+	int lvl = app->scene->player1->lvl;
+	int prevLvl = 0;
+	if (lvl != 0) int prevLvl = lvl - 1;
+
+	char str[24] = {};
+	sprintf(str, "Level Up! %d to %d", prevLvl, lvl);
+	upgradeText->SetString(str, BROWN);
+
+	levelUpBool = false;
+
+	plusExp = exp; //Experience given by the enemy
+	actualExp = app->scene->player1->exp; //Actual player experience
+
+	int newExp = actualExp + plusExp; //Sum of the actual experience plus the experience given by the enemy
+
+	maxExp = floor(pow((float)app->scene->player1->lvl, 1.25f));
+	
+	if (newExp > maxExp)
+	{
+		app->scene->player1->lvl++;
+		int diffExp = newExp - maxExp;
+		newExp = diffExp;
+		levelUpBool = true;
+	}
+	else if (newExp == maxExp)
+	{
+		app->scene->player1->lvl++;
+		newExp = 0;
+		levelUpBool = true;
+	}
+
+	if (levelUpBool)
+	{
+		Player* p = app->scene->player1;
+
+		p->health += ((p->lvl - 1) / 2) + 10
+
+		p = nullptr;
+	}
 }
 
 void LevelUp::Restart()
 {
 	app->tex->UnLoad(interfaceTexture);
+	app->tex->UnLoad(barTexture);
 }
 
 void LevelUp::Update()
 {
 	UpdateButtons();
-	UpdateText();
 }
 
 void LevelUp::Draw()
@@ -48,62 +89,53 @@ void LevelUp::Draw()
 
 	DrawText();
 
-	app->guiManager->DrawCursor();
+	DrawBar({600, 400}, app->scene->player1->exp, maxExp, BLUE);
 
-	
+	//app->guiManager->DrawCursor();
 }
 
-void LevelUp::SetButtons()
+void LevelUp::DrawBar(iPoint pos, int current, int max, SDL_Color color)
 {
-	
+	int size = 261;
+	int thickness = 20;
+	int percent = 1;
+
+	if (current > 0 && max > 0) percent = current / max;
+
+	app->render->DrawRectangle({ pos.x,pos.y,size * percent,thickness }, BLUE, true, false);
+	app->render->DrawTexture(barTexture, pos.x, pos.y, 1, false, &expBarRect, 0, INT_MAX, INT_MAX, SDL_FLIP_NONE, false);
+
 }
 
 void LevelUp::UpdateButtons()
 {
-	upgradeHealthButton->UpdateOnClick(1.0f);
-	upgradeAttackButton->UpdateOnClick(1.0f);
-	upgradeDefenseButton->UpdateOnClick(1.0f);
-	upgradeSpeedButton->UpdateOnClick(1.0f);
-	upgradeLuckButton->UpdateOnClick(1.0f);
-	upgradeStabButton->UpdateOnClick(1.0f);
+	skipButton->Update(1.0f);
 }
 
 void LevelUp::SetText()
 {
-	if (upgradePointsText == nullptr)
+	if (upgradeText == nullptr)
 	{
-		upgradePointsText = (GuiString*)app->guiManager->CreateGuiControl(GuiControlType::TEXT);
-		upgradePointsText->bounds = { 1100,200, 300, 100 };
-		upgradePointsText->SetTextFont(app->fontTTF->inventoryFont);
+		upgradeText = (GuiString*)app->guiManager->CreateGuiControl(GuiControlType::TEXT);
+		upgradeText->bounds = { 600, 200, 300, 100 };
+		upgradeText->SetTextFont(app->fontTTF->inventoryFont);
 
-		char str[5] = {};
-		sprintf(str, "%d", app->scene->player1->featherCount);
-		upgradePointsText->SetString(str, BROWN);
+		int lvl = app->scene->player1->lvl;
+		int prevLvl = 0;
+		if (lvl != 0) int prevLvl = lvl - 1;
+
+		char str[24] = {};
+		sprintf(str, "Level Up! %d to %d", prevLvl, lvl);
+		upgradeText->SetString(str, BROWN);
 	}
-}
-
-void LevelUp::UpdateText()
-{
-	char str[5] = {};
-	sprintf(str, "%d", app->scene->player1->featherCount);
-	upgradePointsText->SetString(str, BROWN);
 }
 
 void LevelUp::DrawText()
 {
-	upgradePointsText->Draw();
+	if (levelUpBool) upgradeText->Draw();
 }
 
 void LevelUp::DrawButtons()
 {
-	LOG("Drawing lvl buttons...");
-
-	upgradeHealthButton->Draw(1, false, false,ButtonType::UPGRADE);
-	upgradeDefenseButton->Draw(1, false, false, UPGRADE);
-	upgradeAttackButton->Draw(1, true, false, UPGRADE);
-	upgradeSpeedButton->Draw(1, true, false, UPGRADE);
-	upgradeLuckButton->Draw(1, true, false, UPGRADE);
-	upgradeStabButton->Draw(1, true, false, UPGRADE);
-
-	
+	skipButton->Draw(1, false);
 }
