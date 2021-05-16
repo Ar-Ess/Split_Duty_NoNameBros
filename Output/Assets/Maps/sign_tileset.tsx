baa0cd6d03a96c06de1c94e13b42357110bd13bc
BooStarGamer <marti.buxeda@gmail.com>
2021-05-16 19:58:30 +0200

    Solved

--- Output/Assets/Maps/sign_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="sign_tileset" tilewidth="16" tileheight="16" tilecount="9" columns="3">
- <image source="sign.png" width="56" height="56"/>
-</tileset>
